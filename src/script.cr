c8f54de27d4005d6a562f3524e10024b6bb7c674
Oleh Prypin <oleh@pryp.in>
2021-02-25 00:56:31 +0100

    Support running shell commands in Windows postinstall

--- src/script.cr
+++ src/script.cr
@@ -6,6 +6,12 @@ module Shards
     def self.run(path, command, script_name, dependency_name)
       Dir.cd(path) do
         output = IO::Memory.new
+        {% if flag?(:win32) %}
+          if chopped = command.lchop?("./")
+            command = ".\\#{chopped}"
+          end
+          command = "cmd /c #{command}"
+        {% end %}
         status = Process.run(command, shell: true, output: output, error: output)
         raise Error.new("Failed #{script_name} of #{dependency_name} on #{command}:\n#{output.to_s.rstrip}") unless status.success?
       end
